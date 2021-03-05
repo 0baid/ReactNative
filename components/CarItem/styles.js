1bca652e3e3da8f38648df2f334ae1c7f2578774
Obaid <obaidurrehman555@gmail.com>
2021-03-05 20:08:25 +0500

    modified:   App.js 	new file:   assets/cars.js 	new file:   assets/images/Model3.jpeg 	new file:   assets/images/ModelS.jpeg 	new file:   assets/images/ModelX.jpeg 	new file:   assets/images/ModelY.jpeg 	new file:   assets/images/SolarPanels.jpeg 	new file:   assets/images/SolarRoof.jpeg 	new file:   assets/images/logo.png 	new file:   assets/images/menu.png 	new file:   components/CarItem/index.js 	new file:   components/CarItem/styles.js

---
+++ components/CarItem/styles.js
@@ -0,0 +1,34 @@
+import {StyleSheet} from 'react-native'
+
+export const styles = StyleSheet.create({
+    carContainer:{
+      width:'100%',
+      height:'100%',
+      
+    },
+    image:{
+      width:'100%',
+      height:'100%',
+      resizeMode:'cover',
+      position:'absolute',
+      
+    },
+  
+    titles:{
+      marginTop:'30%',
+      width:'100%',
+      alignItems:'center'
+    },
+  
+    title:{
+      fontSize:40,
+      fontWeight:'bold',
+    },
+    subtitle:{
+      fontSize:20,
+      color:'#5c5362'
+    }
+    
+  
+  });
+  
